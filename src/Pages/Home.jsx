c0a90f392b4252ed688ad8e055b25888636e837c
Samundeeswari <samundeeswari367@gmail.com>
2025-03-10 20:12:31 +0530

    Adding all files of frontend to git

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/Food.png" class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/Briyani.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Briyani</h5>
+        <p class="card-text">Biryani is a flavorful, layered rice dish that's popular in South Asia. It's often made with meat or seafood, but can also be vegetarian. </p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/Panner.png" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Panner Butter Masala</h5>
+        <p class="card-text">Paneer Butter Masala, also known as butter paneer is a rich & creamy curry made with paneer, spices, onions, tomatoes, cashews and butter. As the name denotes, the curry is cooked in butter which imparts it’s characteristic buttery flavor to the dish.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/Laddu.png" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Laddu</h5>
+        <p class="card-text">TSpherical sweet from the Indian subcontinentImage courtesy of freepik.com
+freepik.com
+Laddu or laddoo is a spherical sweet from the Indian subcontinent made of various ingredients and sugar syrup or jaggery.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/Parotta.png" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Parrota</h5>
+        <p class="card-text">A parotta is a layered, flaky flatbread originating from the Malabar region of South India, typically made from refined flour (maida), and often served with meat curries like chicken, beef, or goat.</p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+    </div>
+  )
+}
+
+export default Home
